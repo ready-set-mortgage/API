d31d82f749f00a0dd212ca551ca727833b6bd6db
sohalsdr <sohal@sohalsdr.com>
2023-11-05 09:24:15 -0600

    Manually specify allowed origins

--- api.py
+++ api.py
@@ -25,7 +25,7 @@ import openai
 #------------------------#
 
 app = FastAPI()                                     # Creates FastAPI App
-origins = ["*"]
+origins = ["http://readysetmortgage.co", "https://readysetmortgage.co", "*"]
 
 app.add_middleware(
     CORSMiddleware,
