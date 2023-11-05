f7cb689b857b00b187d2cba4e921cab6a163bfaa
sohalsdr <sohal@sohalsdr.com>
2023-11-05 01:53:35 -0500

    Added readiness logic

--- api.py
+++ api.py
@@ -63,7 +63,7 @@ class UserStats(BaseModel):
 #   SANITY CHECK ENDPOINT   #
 #---------------------------#
 
-@app.get("/")                            # Sanity check endpoint to ensure server is accessible
+@app.get("/")                                       # Sanity check endpoint to ensure server is accessible
 async def sanity_check():
     return {
         "message": "Welcome to Ready, Set, Mortgage's API! To read more about the available endpoints, visit https://api.readysetmortgage.co/docs"
@@ -75,27 +75,33 @@ async def sanity_check():
 #---------------------------------#
 
 @app.post("/readiness")
-async def readiness(userstats: UserStats):
-    notices = []
+async def readiness(userstats: UserStats):                                  # Declares an empty List of notices
+
+    monthly_debt = calculate_monthly_debt(userstats.monthly_car_payment, userstats.monthly_credit_card_payment, userstats.est_monthly_mortgage_payment, userstats.student_loan_payment)
+    credit_score = userstats.credit_score
+    ltv = calculate_ltv(userstats.home_appraised_value, userstats.down_payment_amount)
+    dti = calculate_dti(monthly_debt, userstats.gross_monthly_income)
+    fedti = calculate_fedti(userstats.est_monthly_mortgage_payment, userstats.gross_monthly_income)
+    notices = assess_notices(credit_score, dti, ltv, fedti)
 
     return {
-        'readiness': 'Placeholder',
+        'readiness': compare_readiness(credit_score, dti, ltv, fedti),
         'breakdown': {
             'credit': {
-                'individual_readiness': 'Placeholder',
-                'score': -1.0
+                'individual_readiness': compare_credit(credit_score),
+                'score': credit_score
             },
             'ltv': {
-                'individual_readiness': 'Placeholder',
-                'score': -1.0
+                'individual_readiness': compare_ltv(ltv),
+                'score': ltv
             },
             'dti': {
-                'individual_readiness': 'Placeholder',
-                'score': -1.0
+                'individual_readiness': compare_dti(dti),
+                'score': dti
             },
             'fedti': {
-                'individual_readiness': 'Placeholder',
-                'score': -1.0
+                'individual_readiness': compare_fedti(fedti),
+                'score': fedti
             }
         },
         'notices': notices
@@ -111,3 +117,91 @@ async def ai_feedback(userstats: UserStats):
     return {
         'response': 'Placeholder'
     }
+
+#----------------------#
+#   HELPER FUNCTIONS   #
+#----------------------#
+
+def calculate_monthly_debt(monthly_car_payment: float, monthly_credit_card_payment: float, est_monthly_mortgage_payment: float, student_loan_payment: float) -> float:
+    return monthly_car_payment + monthly_credit_card_payment + est_monthly_mortgage_payment + student_loan_payment
+
+def calculate_ltv(home_appraised_value: float, down_payment_amount: float) -> float:
+    return (home_appraised_value - down_payment_amount) / home_appraised_value
+
+def calculate_dti(monthly_debt: float, gross_monthly_income: float) -> float:
+    return monthly_debt / gross_monthly_income
+
+def calculate_fedti(est_monthly_mortgage_payment: float, gross_monthly_income: float) -> float:
+    return est_monthly_mortgage_payment / gross_monthly_income
+
+def compare_ltv(ltv: float) -> str:
+    if ltv < 0.80:
+        return 'Y'
+    elif ltv <= 0.95:
+        return 'M'
+    else:
+        return 'N'
+
+def compare_dti(dti: float) -> str:
+    if dti <= 0.36:
+        return 'Y'
+    elif dti <= 0.43:
+        return 'M'
+    else:
+        return 'N'
+
+def compare_fedti(fedti: float) -> str:
+    if fedti <= 28:
+        return 'Y'
+    else:
+        return 'N'
+
+def compare_credit(credit_score) -> str:
+    if credit_score >= 640:
+        return 'Y'
+    else:
+        return 'N'
+
+def compare_readiness(credit_score, dti, ltv, fedti):
+    individual_readiness = {
+        'credit': compare_credit(credit_score),
+        'ltv': compare_ltv(ltv),
+        'dti': compare_dti(dti),
+        'fedti': compare_fedti(fedti)
+    }
+
+    if 'N' in individual_readiness.values():
+        return 'N'
+    elif 'M' in individual_readiness.values():
+        return 'M'
+    else:
+        return 'Y'
+
+
+def assess_notices(credit_score, dti, ltv, fedti) -> list:
+    individual_readiness = {
+        'credit': compare_credit(credit_score),
+        'ltv': compare_ltv(ltv),
+        'dti': compare_dti(dti),
+        'fedti': compare_fedti(fedti)
+    }
+
+    notices = []
+
+    if individual_readiness['credit'] == 'N':
+        notices.append("Your credit score is too low. Lenders will generally only accept a credit score greater than 640")
+    
+    if individual_readiness['ltv'] == 'N':
+        notices.append("The ratio of your loan amount to the house's value is very high. Loan-to-value ratios above 95\% tend to be rejected by lenders. Consider increasing your down payment.")
+    elif individual_readiness['ltv'] == 'M':
+        notices.append("The ratio of your loan amount to the house's value is moderately high. Loan-to-value ratios between 80% and 95\% tend to result in higher interest rates and may require you to purchase private mortgage insurance. Consider increasing your down payment.")
+    
+    if individual_readiness['dti'] == 'N':
+        notices.append("Your ratio of debt to income is too high to be accepted by a lender. The highest ratio any lender will take is 43\%, but most lenders prefer a ratio of 36% or less.")
+    elif individual_readiness['dti'] == 'M':
+        notices.append("Your ratio of debt to income is moderately high. While some lenders may accept it, most lenders prefer a debt-to-income ratio of 36% or less.")
+    
+    if individual_readiness['fedti'] == 'N':
+        notices.append("The percentage of that debt going toward mortgages, your \"front-end debt\", is too high to be considered for a loan. Generally, the ratio of your front-end debt to income should not exceed 28%.")
+    
+    return notices
